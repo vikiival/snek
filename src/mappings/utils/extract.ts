9d474fc1137b79431db33b7651c618099157cd48
Viki Val <viktorko99@gmail.com>
2021-08-27 17:15:12 +0200

    Refactored

--- src/mappings/utils/extract.ts
+++ src/mappings/utils/extract.ts
@@ -6,19 +6,29 @@ import { BasicExtrinsicData, CollectionData } from './types';
 
 export const log = (title: string, arg: any) => logger.info(`[${title}] ${JSON.stringify(arg, null, 2)}` )
 
-export const getCollectionEvents = (records: EventRecord[]): string | undefined => {
-  const eventRecord = records.find(({ event }) => matchEvent(event, 'TokenClassCreated', 'nft'));
+export const getEvents = (records: EventRecord[], cb: (record: EventRecord) => boolean): string[] => {
+  const eventRecord = records.find(cb);
   if (eventRecord) {
-    const topics = getArgs(eventRecord.event.data);
-    return topics[1];
+    return getArgs(eventRecord.event.data);
   }
+
+  return []
 }
+
+const getCollectionEvents = (records: EventRecord[]): string | undefined => {
+  return getEvents(records, isTokenClassCreated)[1];
+}
+
+const isTokenClassCreated = ({ event }:EventRecord): boolean => matchEvent(event, 'TokenClassCreated', 'nft');
 
 export const matchEvent = (event: Event, method: string, section: string): boolean => {
   return event.method.toString() === method && event.section.toString() === section;
 }
 
-export const isCreateCollection = (call: TCall): boolean => call.section === "nft" && call.method === "createClass"
+export const isCreateCollection = (call: TCall): boolean => isExtrinsic(call, "createClass", "nft");
+export const isCreateToken = (call: TCall): boolean => isExtrinsic(call, "mint", "nft");
+
+export const isExtrinsic = (call: TCall, method: string, section: string): boolean => call.section === section && call.method === method
 
 export const getArgs = (args: Codec[]): string[] => {
   // logger.info(`getArgs ${args.toString()}`)
@@ -44,6 +54,25 @@ export const getBasicData = (extrinsic: SubstrateExtrinsic): BasicExtrinsicData 
 export const processCollection = (extrinsic: SubstrateExtrinsic): CollectionData => {
   if (!isCreateCollection(extrinsic.extrinsic.method as TCall)) {
     logger.error(`[COLLECTION] ${extrinsic.extrinsic.method.toString()} is not a create collection`);
+    return;
+  }
+
+  const data = getBasicData(extrinsic);
+  const args = getArgs(extrinsic.extrinsic.args);
+  const id = getCollectionEvents(extrinsic.events);
+
+  return {
+    ...data,
+    id,
+    metadata: args[0],
+  }
+
+}
+
+
+export const processToken = (extrinsic: SubstrateExtrinsic): CollectionData => {
+  if (!isCreateToken(extrinsic.extrinsic.method as TCall)) {
+    logger.error(`[TOKEN] ${extrinsic.extrinsic.method.toString()} is not a create NFT`);
     return;
   }
 
