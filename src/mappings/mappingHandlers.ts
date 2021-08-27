a5f4825ee1765a50f4064a7fbe0443b4068bf142
Viki Val <viktorko99@gmail.com>
2021-08-27 16:47:32 +0200

    Working handler for collection

--- src/mappings/mappingHandlers.ts
+++ src/mappings/mappingHandlers.ts
@@ -1,9 +1,10 @@
 import { SubstrateExtrinsic } from "@subql/types";
 import { CollectionEntity } from "../types";
-import { processCollection } from './utils/extract';
+import { processCollection, log } from './utils/extract';
 
 export async function handleCreateCollection(extrinsic: SubstrateExtrinsic): Promise<void> {
     const collection = processCollection(extrinsic)
+    log('CREATE', collection)
     if (!collection.id) {
         logger.warn("No collection ID found in extrinsic");
         return;
--- src/mappings/utils/extract.ts
+++ src/mappings/utils/extract.ts
@@ -1,23 +1,28 @@
 import { Call as TCall } from "@polkadot/types/interfaces";
-import { EventRecord } from '@polkadot/types/interfaces';
+import { EventRecord, Event } from '@polkadot/types/interfaces';
 import { SubstrateExtrinsic } from "@subql/types";
 import { Codec } from '@polkadot/types/types';
 import { BasicExtrinsicData, CollectionData } from './types';
 
+export const log = (title: string, arg: any) => logger.info(`[${title}] ${JSON.stringify(arg, null, 2)}` )
+
 export const getCollectionEvents = (records: EventRecord[]): string | undefined => {
-  const eventRecord = records.find(record => record.event.method.toString() === "TokenClassCreated");
-  logger.info('parsing event', eventRecord.toString())
+  const eventRecord = records.find(({ event }) => matchEvent(event, 'TokenClassCreated', 'nft'));
   if (eventRecord) {
-    const topics = eventRecord.topics.map(topic => topic.toString());
+    const topics = getArgs(eventRecord.event.data);
     return topics[1];
   }
+}
+
+export const matchEvent = (event: Event, method: string, section: string): boolean => {
+  return event.method.toString() === method && event.section.toString() === section;
 }
 
 export const isCreateCollection = (call: TCall): boolean => call.section === "nft" && call.method === "createClass"
 
 export const getArgs = (args: Codec[]): string[] => {
-  logger.info('getArgs', args.toString())
-  return args.map(String);
+  // logger.info(`getArgs ${args.toString()}`)
+  return args.map(arg => arg.toHuman().toString());
 }
 
 export const getBasicData = (extrinsic: SubstrateExtrinsic): BasicExtrinsicData => {
